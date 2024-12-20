51a1d6b343569ddc1c2efc5c34cae6c17bbfd548
Maxim <Safronovmaxiim@mail.ru>
2024-12-20 23:52:32 +0300

    adding logic for mobile (320) carousel brand cards

--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
-const carouselItems = document.querySelectorAll('.services__list-item');
-const points = document.querySelectorAll('.carousel__list-points-point');
+const cards = document.querySelectorAll('.carousel__list-item')
+const dots = document.querySelectorAll('.carousel__list-points-point');
+const carouselList = document.querySelector('.carousel__list');
 
-points.forEach((dot,index) => {
-    let dotIndex = index;
+const shadowLine = document.querySelector('.services__carousel-shadow');
+let currentIndex = 0;
+
+function carouselBrand(index) {
+    const maxIndex = cards.length - 1;
+    const offset = -index * 80;
+    carouselList.style.transform = `translateX(${offset}%)`;
+
+
+    dots.forEach((dot, i) => {
+        if (i === index) {
+            dot.classList.add('carousel__list-points-point__active');
+        } else {
+            dot.classList.remove('carousel__list-points-point__active');
+        }
+
+        if (maxIndex === index && index === maxIndex) {
+            shadowLine.style.display = "none"
+        } else {
+            shadowLine.style.display = ""
+        }
+    })
+};
+
+dots.forEach((dot, index) => {
     dot.addEventListener('click', () => {
-        carouselItems.forEach((brand,index) => {
-            let indexBrand = index;
-            if(dotIndex === indexBrand ){
-                
-            }
-        })
+        currentIndex === index;
+        carouselBrand(index);
     })
 })
+
+carouselBrand(currentIndex)
