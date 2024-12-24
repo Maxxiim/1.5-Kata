5ee755cc9c3b37e63be48c18075c410a6d8c2431
Maxim <Safronovmaxiim@mail.ru>
2024-12-24 15:40:53 +0300

    carousel brand for adaptive(320) 1.1

--- js/main.js
+++ js/main.js
@@ -1,101 +1,41 @@
-// const carousel = document.querySelector('.carousel__list');
-// const dots = document.querySelectorAll('.carousel__list-points-point');
-// const items = document.querySelectorAll('.carousel__list-item');
+const carousel = document.querySelector('.carousel__list');
+const dots = document.querySelectorAll('.carousel__list-points-point');
+const item = document.querySelector('.carousel__list-item').offsetWidth;
 
-// let currentIndex = 0;
-// const itemWidth = items[0].offsetWidth + 16;
-// const totalItems = items.length;
+let currentIndex = 0;
 
-// const appendClones = () => {
-//     items.forEach((item) => {
-//         const cloneStart = item.cloneNode(true); 
-//         const cloneEnd = item.cloneNode(true); 
-//         carousel.appendChild(cloneEnd); 
-//         carousel.prepend(cloneStart); 
-//     });
-// };
 
-// appendClones();
+function updateCarousel(){
+    const offset = -(currentIndex * (item));
+    carousel.computedStyleMap.transform = carousel.style.transform = `translateX(${offset}px)`;
+};
 
-// currentIndex = totalItems;
-// carousel.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+function handleInfiniteScroll() {
+    const firstItem = carousel.firstElementChild;
+    const lastItem = carousel.lastElementChild;
 
-// const updateCarousel = () => {
-//     const offset = currentIndex * itemWidth;
-//     carousel.style.transition = 'transform 0.5s ease-in-out';
-//     carousel.style.transform = `translateX(-${offset}px)`;
 
-//     dots.forEach((dot, index) => {
-//         if (index === (currentIndex % totalItems)) {
-//             dot.classList.add('carousel__list-points-point__active');
-//         } else {
-//             dot.classList.remove('carousel__list-points-point__active');
-//         }
+    if (currentIndex === dots.length) {
+        carousel.appendChild(firstItem);
+        currentIndex = 0;
+    } else if( currentIndex < 0){
+        carousel.insertBefore(lastItem, firstItem);
+        currentIndex = dots.length - 1;
+    }
 
-//     });
-// };
+    updateCarousel();
+};
 
-// carousel.addEventListener('transitionend', () => {
-//     if (currentIndex === totalItems * 2) {
-//         carousel.style.transition = 'none';
-//         currentIndex = totalItems;
-//         carousel.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
-//     } else if (currentIndex === totalItems - 1) {
-//         carousel.style.transition = 'none';
-//         currentIndex = totalItems - 1 + totalItems;
-//         carousel.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
-//     }
-// });
-
-// dots.forEach((dot, index) => {
-//     dot.addEventListener('click', () => {
-//         currentIndex = totalItems + index;
-//         updateCarousel();
-//     });
-// });
-
-// let startX = 0;
-// let isDragging = false;
-// let currentTranslate = 0;
-// let prevTranslate = -currentIndex * itemWidth;
-
-// carousel.addEventListener('touchstart', (event) => {
-//     startX = event.touches[0].clientX;
-//     isDragging = true;
-//     carousel.style.transition = 'none';
-// });
-
-// carousel.addEventListener('touchmove', (event) => {
-//     if (!isDragging) return;
-
-//     const touchX = event.touches[0].clientX;
-//     const deltaX = touchX - startX;
-
-//     currentTranslate = prevTranslate + deltaX;
-//     carousel.style.transform = `translateX(${currentTranslate}px)`;
-// });
-
-// carousel.addEventListener('touchend', () => {
-//     isDragging = false;
-
-//     const movedBy = currentTranslate - prevTranslate;
-
-//     if (movedBy < -50) {
-//         currentIndex++;
-//     } else if (movedBy > 50) {
-//         currentIndex--;
-//     }
-
-//     prevTranslate = -currentIndex * itemWidth;
-//     updateCarousel();
-// });
-
-// window.addEventListener('resize', () => {
-//     prevTranslate = -currentIndex * itemWidth;
-//     updateCarousel();
-// });
-
-// updateCarousel();
+dots.forEach((dot, i) => {
+    dot.addEventListener('click', () => {
+        dots.forEach((d) => {
+            d.classList.remove('carousel__list-points-point__active')
+        })
+        currentIndex = i;
+        dot.classList.add('carousel__list-points-point__active');
+        updateCarousel();
+    })
+})
 
 
 // =======================================
